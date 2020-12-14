727a2dd3091e1495b21488c20f662e9e20c1e350
Gamsched <41441660+Gamsched@users.noreply.github.com>
2020-12-14 14:55:31 +0500

    preload js

--- js/script.js
+++ js/script.js
@@ -12,5 +12,17 @@ $(document).ready(function(){
   
     // маска ввода
   $('#phone').inputmask("+\\9\\98(99)999-99-99");
+    
+  // music
+  var mySong = document.getElementById("mySong");
+  var icon = document.getElementById("icon");
+    
+  // preloader
+  document.querySelector('.btn__preloader').onclick = function(){
+      document.querySelector('.preloader').style.display = 'none';
+      document.querySelector('.preloader').style.transition = 'opacity 0.3s';
+      mySong.play();
+  }
 
+  
 });
